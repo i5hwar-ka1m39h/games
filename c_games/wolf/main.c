10874247f5c7cddc8d82ad244ef92f801994aa4e
ishwar_kalmegh <ishwarkalmegh156@gmail.com>
2026-08-03 21:49:52 +0530

    feat: now i can draw shit created a boundry

--- c_games/wolf/main.c
+++ c_games/wolf/main.c
@@ -9,13 +9,15 @@
 #include <SDL2/SDL_surface.h>
 #include <SDL2/SDL_timer.h>
 #include <SDL2/SDL_video.h>
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <wchar.h>
-#include <stdbool.h>
 
 #define SCREEN_HEIGHT 720
 #define SCREEN_WIDTH 1280
+#define MAZE_COL 20
+#define MAZE_ROW 20
 
 typedef struct {
 
@@ -23,6 +25,62 @@ typedef struct {
   SDL_Window *wndw;
 } App;
 
+typedef struct {
+  int x;
+  int y;
+} PlayerPos;
+
+int maze[MAZE_ROW][MAZE_COL] = {
+    {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+    {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+    {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+    {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 1},
+    {1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 1},
+    {1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+    {1, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+    {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+    {1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+    {1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+    {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+    {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+    {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+    {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+    {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+    {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+    {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+    {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+    {1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+    {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1}};
+
+bool Is_wall(int row, int col) {
+  if (col < 0 || col >= MAZE_COL || row < 0 || row >= MAZE_ROW) {
+    return true;
+  }
+  return maze[row][col] == 1;
+}
+void RenderBlock(App *app) {
+  int block_height = SCREEN_HEIGHT / MAZE_ROW;
+  int block_width = SCREEN_WIDTH / MAZE_COL;
+
+  for (int i = 0; i < MAZE_ROW; i++) {
+    for (int j = 0; j < MAZE_COL; j++) {
+      SDL_Rect block = {i * block_width, j * block_height, block_width,
+                        block_height};
+
+			Uint8 col;
+			if(Is_wall(i, j)){
+					col = 255;
+			}else{
+				col = 0;
+			}
+      SDL_SetRenderDrawColor(app->rndr, col, 0, 0, 255);
+
+      SDL_RenderFillRect(app->rndr, &block);
+    }
+  }
+
+  SDL_RenderPresent(app->rndr);
+}
 int main() {
 
   App app;
@@ -35,50 +93,43 @@ int main() {
       SDL_CreateWindow("chikat", SDL_WINDOWPOS_UNDEFINED,
                        SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, 0);
 
-	if(!app.wndw){
-		printf("error occured while creating window %s", SDL_GetError());
-		exit(1);
-	}
+  if (!app.wndw) {
+    printf("error occured while creating window %s", SDL_GetError());
+    exit(1);
+  }
 
+  app.rndr = SDL_CreateRenderer(app.wndw, -1, SDL_RENDERER_ACCELERATED);
 
-	app.rndr = SDL_CreateRenderer(app.wndw, -1 , SDL_RENDERER_ACCELERATED);
+  if (!app.rndr) {
+    printf("error creating render %s", SDL_GetError());
+    exit(1);
+  }
 
-	if(!app.rndr){
-		printf("error creating render %s", SDL_GetError());
-		exit(1);
-	}
+  SDL_Surface *surface = SDL_GetWindowSurface(app.wndw);
 
-	SDL_Surface* surface = SDL_GetWindowSurface(app.wndw);
+  SDL_FillRect(surface, NULL, SDL_MapRGB(surface->format, 0xFF, 0xFF, 0xFF));
 
-	SDL_FillRect(surface, NULL,SDL_MapRGB(surface->format, 0xFF,0xFF,0xFF));
-
-	SDL_UpdateWindowSurface(app.wndw
-			 );
+  SDL_UpdateWindowSurface(app.wndw);
 
   SDL_RenderClear(app.rndr);
-  SDL_SetRenderDrawColor(app.rndr, 120, 145, 160, 90);
-
-  SDL_RenderDrawPoint(app.rndr, 50, 60);
- 
-  SDL_Rect rect = {50, 50, 60, 78};
-  SDL_RenderFillRect(app.rndr, &rect);
 
   SDL_RenderPresent(app.rndr);
 
+  RenderBlock(&app);
 
-	SDL_Event e;
-	bool quit = false;
-	while(quit == false){
-		while(SDL_PollEvent(&e)){
-			if(e.type == SDL_QUIT){
-				printf("quited the window");
-				quit = true;
-			}
-		}
-	}
+  SDL_Event e;
+  bool quit = false;
+  while (quit == false) {
+    while (SDL_PollEvent(&e)) {
+      if (e.type == SDL_QUIT) {
+        printf("quited the window");
+        quit = true;
+      }
+    }
+  }
 
-	SDL_DestroyRenderer(app.rndr);
-	SDL_DestroyWindow(app.wndw);
-	SDL_Quit();
+  SDL_DestroyRenderer(app.rndr);
+  SDL_DestroyWindow(app.wndw);
+  SDL_Quit();
   return 0;
 }
