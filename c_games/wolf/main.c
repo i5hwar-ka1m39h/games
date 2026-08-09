5e06f7916d4c6e031c996a648bfaf64a83fcb2bf
ishwar_kalmegh <ishwarkalmegh156@gmail.com>
2026-08-09 21:39:59 +0530

    mod: ray marcher to ray tracer

--- c_games/wolf/main.c
+++ c_games/wolf/main.c
@@ -94,36 +94,265 @@ void RenderPlayer(App *app, PlayerPos *plyr) {
   int endptX = startptX + cos(plyr->angle) * 10;
   int endptY = startptY + sin(plyr->angle) * 10;
 
-  SDL_SetRenderDrawColor(app->rndr, 0, 0, 255, 255);
+  SDL_SetRenderDrawColor(app->rndr, 0, 255, 0, 255);
   SDL_RenderDrawLine(app->rndr, startptX, startptY, endptX, endptY);
 }
 
 void RenderLine(App *app, PlayerPos *plyr) {
 
-    int block_height = SCREEN_HEIGHT / MAZE_ROW;
-    int block_width  = SCREEN_HEIGHT / MAZE_COL;
+  int block_height = SCREEN_HEIGHT / MAZE_ROW;
+  int block_width = SCREEN_HEIGHT / MAZE_COL;
 
-    float rayX = plyr->x + 5.0f;
-    float rayY = plyr->y + 5.0f;
+  float rayX = plyr->x + 5.0f;
+  float rayY = plyr->y + 5.0f;
 
-    float dirX = cosf(plyr->angle);
-    float dirY = sinf(plyr->angle);
+  float dirX = cosf(plyr->angle);
+  float dirY = sinf(plyr->angle);
 
-    while (1) {
+  while (1) {
 
-        // Move the ray forward
-        rayX += dirX;
-        rayY += dirY;
+    // Move the ray forward
+    rayX += dirX;
+    rayY += dirY;
 
-        // Convert screen coordinates to maze coordinates
-        int col = (int)(rayX / block_width);
-        int row = (int)(rayY / block_height);
+    // Convert screen coordinates to maze coordinates
+    int col = (int)(rayX / block_width);
+    int row = (int)(rayY / block_height);
 
-        // Have we hit a wall?
-        if (Is_wall(row, col)) {
-            break;
+    // Have we hit a wall?
+    if (Is_wall(row, col)) {
+      break;
+    }
+  }
+
+  SDL_SetRenderDrawColor(app->rndr, 0, 0, 255, 255);
+
+  SDL_RenderDrawLine(app->rndr, plyr->x + 5, plyr->y + 5, (int)rayX, (int)rayY);
+}
+void RenderLineImpr(App *app, PlayerPos *plyr) {
+
+    const float blockWidth  = (float)SCREEN_WIDTH / MAZE_COL;
+    const float blockHeight = (float)SCREEN_HEIGHT / MAZE_ROW;
+
+    // Player center
+    const float px = (float)plyr->x + 5.0f;
+    const float py = (float)plyr->y + 5.0f;
+
+    // Direction of the ray
+    const float rayDirX = cosf(plyr->angle);
+    const float rayDirY = sinf(plyr->angle);
+
+    /*
+     * ---------------------------------------------------------
+     * HORIZONTAL INTERSECTIONS
+     * ---------------------------------------------------------
+     */
+
+    float horizontalHitX = 0.0f;
+    float horizontalHitY = 0.0f;
+    float horizontalDistance = INFINITY;
+
+    // Don't calculate horizontal intersections when ray is
+    // almost perfectly horizontal.
+    if (fabsf(rayDirY) > 0.0001f) {
+
+        float firstHorizontalY;
+
+        if (rayDirY > 0) {
+            // Ray is pointing DOWN
+
+            firstHorizontalY =
+                floorf(py / blockHeight) * blockHeight
+                + blockHeight;
+        }
+        else {
+            // Ray is pointing UP
+
+            firstHorizontalY =
+                floorf(py / blockHeight) * blockHeight;
+        }
+
+        // How far along the ray until we reach that Y?
+        float distance =
+            (firstHorizontalY - py) / rayDirY;
+
+        // X position at that distance
+        float firstHorizontalX =
+            px + distance * rayDirX;
+
+        float stepY;
+
+        if (rayDirY > 0) {
+            stepY = blockHeight;
+        }
+        else {
+            stepY = -blockHeight;
+        }
+
+        // Every next horizontal grid intersection
+        float currentX = firstHorizontalX;
+        float currentY = firstHorizontalY;
+
+        while (1) {
+
+            // Convert intersection into maze coordinates
+            int col = (int)(currentX / blockWidth);
+
+            int row;
+
+            if (rayDirY > 0) {
+                row = (int)(currentY / blockHeight);
+            }
+            else {
+                row = (int)(currentY / blockHeight) - 1;
+            }
+
+            // Did we leave the maze?
+            if (col < 0 || col >= MAZE_COL ||
+                row < 0 || row >= MAZE_ROW) {
+                break;
+            }
+
+            // Is this cell a wall?
+            if (Is_wall(row, col)) {
+
+                horizontalHitX = currentX;
+                horizontalHitY = currentY;
+
+                horizontalDistance =
+                    sqrtf(
+                        (currentX - px) * (currentX - px) +
+                        (currentY - py) * (currentY - py)
+                    );
+
+                break;
+            }
+
+            currentX += rayDirX * (stepY / rayDirY);
+            currentY += stepY;
         }
     }
+
+
+    /*
+     * ---------------------------------------------------------
+     * VERTICAL INTERSECTIONS
+     * ---------------------------------------------------------
+     */
+
+    float verticalHitX = 0.0f;
+    float verticalHitY = 0.0f;
+    float verticalDistance = INFINITY;
+
+    // Don't calculate vertical intersections when ray is
+    // almost perfectly vertical.
+    if (fabsf(rayDirX) > 0.0001f) {
+
+        float firstVerticalX;
+
+        if (rayDirX > 0) {
+            // Ray is pointing RIGHT
+
+            firstVerticalX =
+                floorf(px / blockWidth) * blockWidth
+                + blockWidth;
+        }
+        else {
+            // Ray is pointing LEFT
+
+            firstVerticalX =
+                floorf(px / blockWidth) * blockWidth;
+        }
+
+        // How far along the ray until we reach that X?
+        float distance =
+            (firstVerticalX - px) / rayDirX;
+
+        // Y position at that distance
+        float firstVerticalY =
+            py + distance * rayDirY;
+
+        float stepX;
+
+        if (rayDirX > 0) {
+            stepX = blockWidth;
+        }
+        else {
+            stepX = -blockWidth;
+        }
+
+        float currentX = firstVerticalX;
+        float currentY = firstVerticalY;
+
+        while (1) {
+
+            int row = (int)(currentY / blockHeight);
+
+            int col;
+
+            if (rayDirX > 0) {
+                col = (int)(currentX / blockWidth);
+            }
+            else {
+                col = (int)(currentX / blockWidth) - 1;
+            }
+
+            // Did we leave the maze?
+            if (row < 0 || row >= MAZE_ROW ||
+                col < 0 || col >= MAZE_COL) {
+                break;
+            }
+
+            // Is this cell a wall?
+            if (Is_wall(row, col)) {
+
+                verticalHitX = currentX;
+                verticalHitY = currentY;
+
+                verticalDistance =
+                    sqrtf(
+                        (currentX - px) * (currentX - px) +
+                        (currentY - py) * (currentY - py)
+                    );
+
+                break;
+            }
+
+            currentX += stepX;
+            currentY += rayDirY * (stepX / rayDirX);
+        }
+    }
+
+
+    /*
+     * ---------------------------------------------------------
+     * CHOOSE THE CLOSEST HIT
+     * ---------------------------------------------------------
+     */
+
+    float hitX;
+    float hitY;
+
+    if (horizontalDistance < verticalDistance) {
+
+        // Horizontal wall is closer
+        hitX = horizontalHitX;
+        hitY = horizontalHitY;
+
+    }
+    else {
+
+        // Vertical wall is closer
+        hitX = verticalHitX;
+        hitY = verticalHitY;
+    }
+
+
+    /*
+     * ---------------------------------------------------------
+     * DRAW RAY
+     * ---------------------------------------------------------
+     */
 
     SDL_SetRenderDrawColor(
         app->rndr,
@@ -135,10 +364,10 @@ void RenderLine(App *app, PlayerPos *plyr) {
 
     SDL_RenderDrawLine(
         app->rndr,
-        plyr->x + 5,
-        plyr->y + 5,
-        (int)rayX,
-        (int)rayY
+        (int)px,
+        (int)py,
+        (int)hitX,
+        (int)hitY
     );
 }
 
@@ -159,10 +388,10 @@ void Moveplayer(PlayerPos *plyr, SDL_Event *ev) {
       plyr->x = plyr->x + 5;
       break;
     case SDLK_LEFT:
-      plyr->angle = plyr->angle - 0.5*(PI/180);
+      plyr->angle = plyr->angle - 0.5 * (PI / 180);
       break;
     case SDLK_RIGHT:
-      plyr->angle = plyr->angle + 0.5*(PI/180);
+      plyr->angle = plyr->angle + 0.5 * (PI / 180);
       break;
     default:
       break;
@@ -202,7 +431,6 @@ int main() {
   RenderBlock(&app);
   RenderPlayer(&app, &plyr);
 
-  RenderLine(&app, &plyr);
   SDL_RenderPresent(app.rndr);
 
   SDL_Event e;
@@ -222,7 +450,6 @@ int main() {
 
     RenderBlock(&app);
     RenderPlayer(&app, &plyr);
-    RenderLine(&app, &plyr);
     SDL_RenderPresent(app.rndr);
   }
 
